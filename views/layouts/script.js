f58b1bb26d26d2a79f37ebfd70ec38b8788cc8e3
Kristopher Keene <kristopherkeene@gmail.com>
2021-02-19 17:32:08 -0500

    Removed node_modules and init commit

---
+++ views/layouts/script.js
@@ -0,0 +1,26 @@
+var chickadeeSong = new Audio();
+chickadeeSong.src = "../../sounds/chickadee.mp3";
+
+var bluejaySong = new Audio();
+bluejaySong.src = "sounds/bluejay.mp3";
+
+var carolinawrenSong = new Audio();
+carolinawrenSong.src = "sounds/carolinawren.mp3";
+
+var woodpeckerSong = new Audio();
+woodpeckerSong.src = 'sounds/woodpecker.mp3';
+
+var goldfinchSong = new Audio();
+goldfinchSong.src = 'sounds/goldfinch.mp3';
+
+var housefinchSong = new Audio();
+housefinchSong.src = 'sounds/housefinch.mp3';
+
+var cardinalSong = new Audio();
+cardinalSong.src = 'sounds/cardinal.mp3';
+
+var robinSong = new Audio();
+robinSong.src = 'sounds/robin.mp3';
+
+var titmouseSong = new Audio();
+titmouseSong.src = 'sounds/tuftedtitmouse.mp3';
